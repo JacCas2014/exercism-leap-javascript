074d24037b255eb3b8d4657394cd8cadeca22d0e
Jac Casey <JacCas2014@users.noreply.github.com>
2016-02-19 11:19:34 +0000

    Jac - changing functionality of isLeap to return false if divisable by 100 but not 400

--- leap.js
+++ leap.js
@@ -2,7 +2,7 @@ function Year (year) {
     this.yearValue = year;
     this.isLeap = function() {
         if(this.yearValue%4 === 0) {
-            if (this.yearValue%100 === 0) {
+            if (this.yearValue%100 === 0 && this.yearValue%400 !== 0) {
                 return false;
             }
             return true;
