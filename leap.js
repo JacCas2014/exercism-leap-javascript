dcce18f04f5fc583f3ecc0b4b46d6d9c6952a197
Jac Casey <JacCas2014@users.noreply.github.com>
2016-02-19 11:23:42 +0000

    Jac - refactoring to not use if statement

--- leap.js
+++ leap.js
@@ -1,14 +1,7 @@
 function Year (year) {
     this.yearValue = year;
     this.isLeap = function() {
-        if(this.yearValue%4 === 0) {
-            if (this.yearValue%100 === 0 && this.yearValue%400 !== 0) {
-                return false;
-            }
-            return true;
-        } else {
-            return false;
-        }
+        return this.yearValue%4 === 0 && this.yearValue%100 !== 0 || this.yearValue%400 === 0;
     }
 }
 
