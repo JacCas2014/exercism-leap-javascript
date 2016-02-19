a4540a383f01aa0bd1e1aae68d7c3270cdcf64fc
Jac Casey <JacCas2014@users.noreply.github.com>
2016-02-19 11:13:01 +0000

    Jac - changed functionality of isLeap to only return true if divisable by 4

--- leap.js
+++ leap.js
@@ -1,10 +1,10 @@
 function Year (year) {
     this.yearValue = year;
     this.isLeap = function() {
-        if(this.yearValue === 2015) {
-            return false;
-        } else if (this.yearValue === 2016) {
+        if(this.yearValue%4 === 0) {
             return true;
+        } else {
+            return false;
         }
     }
 }
