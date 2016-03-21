d5bed5a384035fa0df7a59421b0ff52d07112f12
Jac Casey <JacCas2014@users.noreply.github.com>
2016-03-21 17:31:41 +0000

    Jac - amending code to have isLeap function defined on it's prototype so don't have to define the function every time the constructor is called and provides default isLeap for year instances that don't define an isLeap function

--- leap.js
+++ leap.js
@@ -1,8 +1,9 @@
 function Year (year) {
     this.yearValue = year;
-    this.isLeap = function() {
-        return this.yearValue%4 === 0 && this.yearValue%100 !== 0 || this.yearValue%400 === 0;
-    }
+}
+
+Year.prototype.isLeap = function() {
+    return this.yearValue%4 === 0 && this.yearValue%100 !== 0 || this.yearValue%400 === 0;
 }
 
 module.exports = Year;
